820598d515d49d09cde7ba5f81c80c6604a06198
ivan.kaiser <gringo9991@hotmail.com>
2023-04-05 18:42:13 -0300

    modificado id instancia

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "ec2_instance" {
-  ami           = "ami-0b5eea76982371e91" # Substitua pela AMI desejada
+  ami           = "ami-06e46074ae430fba6" # Substitua pela AMI desejada
   instance_type = "t2.micro"
   key_name      = "key2023" # Substitua pelo nome da sua chave SSH
 }
