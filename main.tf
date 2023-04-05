87e48f21fe8f2ea989c0b2cad010747b0f6e0e2d
ivan.kaiser <gringo9991@hotmail.com>
2023-04-05 18:56:41 -0300

    modificado para bucket

--- main.tf
+++ main.tf
@@ -12,8 +12,12 @@ provider "aws" {
   region = "us-east-1" # define region as per your account
 }
 
-resource "aws_instance" "ec2_instance" {
-  ami           = "ami-06e46074ae430fba6" # Substitua pela AMI desejada
-  instance_type = "t2.micro"
-  key_name      = "key2023" # Substitua pelo nome da sua chave SSH
+resource "aws_s3_bucket" "new_bucket" {
+  bucket = "bucket-valid-teste2"
+
+  object_lock_enabled = false
+
+  tags = {
+    Environment = "Prod"
+  }
 }
