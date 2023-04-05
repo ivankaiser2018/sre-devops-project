5696c56044592acc2e9e85529742b1d56c9af7a6
ivan.kaiser <gringo9991@hotmail.com>
2023-04-05 18:35:31 -0300

    valid ec2 teste

--- main.tf
+++ main.tf
@@ -12,12 +12,8 @@ provider "aws" {
   region = "ap-south-1" # define region as per your account
 }
 
-resource "aws_s3_bucket" "new_bucket" {
-  bucket = "demo-github-action-tf-medium"
-
-  object_lock_enabled = false
-
-  tags = {
-    Environment = "Prod"
-  }
+resource "aws_instance" "ec2_instance" {
+  ami           = "ami-0b5eea76982371e91" # Substitua pela AMI desejada
+  instance_type = "t2.micro"
+  key_name      = "key2023" # Substitua pelo nome da sua chave SSH
 }
